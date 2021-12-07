4f42c9b7b18f47a4bbc2504eca323b47faf4849d
edricklimantoro <edrick.limantoro@my.sampoernauniversity.ac.id>
2021-12-07 22:25:06 +0700

    defined Run() function

--- lib/game.cpp
+++ lib/game.cpp
@@ -1,3 +1,35 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+#include <string>
+
 #include "../includes/tictactoe.h"
 #include "../includes/tttlibrary.h"
 #include "../includes/game.h"
+
+void Game::Run() {
+    srand((unsigned int)time(NULL));
+    std::string command;
+
+    bool play = true;
+    while (play) {
+        Game::PrintCommand();
+		std::cout << std::endl << "Select command: ";
+		std::getline(std::cin, command);
+
+		if (command[0] == '1') {
+			Game::Play(SINGLE_PLAYER);
+			play = Game::ReplayConfirmation();
+		}
+		else if (command[0] == '2') {
+			Game::Play(MULTI_PLAYER);
+			play = Game::ReplayConfirmation();
+		}
+		else if (command[0] == '3') {
+			play = false;
+		}
+		else {
+			std::cout << "Invalid command! Try again..." << std::endl << std::endl;
+		}
+    }
+}
