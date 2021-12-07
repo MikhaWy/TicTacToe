be27d48e1538acf9e1a5a287d5a32c92a4a643d7
MikhaWy <mikhakelaya02@gmail.com>
2021-12-07 21:38:48 +0700

    game.h progress

--- includes/game.h
+++ includes/game.h
@@ -1,6 +1,19 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include "tictactoe.h"
+#include "tttlibrary.h"
+
+class Game {
+public:
+	static void Run();
+
+	static void Play(const int& mode);
+
+	static void PrintCommand();
+
+	static bool ReplayConfirmation();
+};
 
 
 #endif
